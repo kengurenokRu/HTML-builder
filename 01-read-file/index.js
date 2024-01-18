0eac1b44e74b42d1899f5c853fd00ee375441ad5
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-18 17:16:55 +0300

    feat: add full solution 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,13 @@
-const path = require('node:path'); 
+const path = require('node:path');
 const fs = require('node:fs');
-const file = new fs.ReadStream(path.format({dir: __dirname,base: 'text.txt'}));
-
-file.on('readable', function() {
-    let data = file.read();
-    if (data !== null )
-    console.log(String(data));
-else 
-console.log("Данных нет");
+const file = new fs.ReadStream(path.format({ dir: __dirname, base: 'text.txt' }));
+let count = 0;
+file.on('readable', function () {
+    let data;    
+    while ((data = file.read()) != null) 
+    {
+        console.log(String(data));
+        count++;        
+    }
+    if (count == 0) console.log("Файл пуст");
 });
