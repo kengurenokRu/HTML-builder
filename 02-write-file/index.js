ec83524c7a79abb15b5663f41226553cad2e71db
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-18 17:53:54 +0300

    feat: add part of solution 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,19 @@
 console.log("Приветствую тебя, путник!");
-console.log("Что желаешь записать в файл?");
-
 const path = require('node:path');
 const fs = require('node:fs');
 const file = new fs.WriteStream(path.format({ dir: __dirname, base: '02-write-file', ext: ".txt"}));
-/*let str = "";
-while (str!= "exit" || str != "ctrl + c")
-{
+const readline = require('readline');
+let text = readline.createInterface(process.stdin, process.stdout);
 
-}*/
+    text.setPrompt('Что желаешь записать в файл?');
+    text.prompt();
+    text.on('line', (input) => {
+        file.write(input);
+        str = input;
+        text.close();
+    });
+
+
+ 
+
+
