bf36b5a4c4d67662dcce77e77ca91ae3271ae8bd
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-18 20:02:04 +0300

    feat: add full solution 02-write-fine

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,18 +2,24 @@ console.log("Приветствую тебя, путник!");
 const path = require('node:path');
 const fs = require('node:fs');
 const file = new fs.WriteStream(path.format({ dir: __dirname, base: '02-write-file', ext: ".txt"}));
-const readline = require('readline');
-let text = readline.createInterface(process.stdin, process.stdout);
-
-    text.setPrompt('Что желаешь записать в файл?');
+const readline = require('readline'); 
+    let text = readline.createInterface(process.stdin, process.stdout);
+    text.setPrompt('Что желаешь записать в файл? (признак окончания ввода комбинация клавиш ctrl+c или слово exit)\n');
     text.prompt();
     text.on('line', (input) => {
-        file.write(input);
-        str = input;
+        if (input != "exit")
+        {
+            file.write(input);
+            file.write("\n");
+        }
+    else 
+    {       
         text.close();
+        console.log("Прощай, Путник! Надеюсь, наша совместная работа тебе понравилась");
+    }
     });
 
-
+    
  
 
 
