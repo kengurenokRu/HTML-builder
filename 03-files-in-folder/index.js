dd0d550182497151567ef9f20821e903601212cb
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-19 10:36:55 +0300

    feat: add all solution 03-file-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,19 @@
 const path = require('node:path');
 const myPath = path.resolve(__dirname, "./secret-folder");
 const dir = require('node:fs').promises;
+const fs = require('fs');
 const files = dir.readdir(myPath, { withFileTypes: true });
-let count = -1;
 let str = [];
 let filenames = [];
-let k = -1;
 files.then(function file(data) {
     data.forEach(function f(file) {
-        count++;
-        str.push(path.basename(file.name) + "-" + path.extname(file.name) + "-");
+        str.push(path.basename(file.name, path.extname(file.name)) + " - " + path.extname(file.name).slice(1) + " - ");
         filenames.push(path.basename(file.name));
     });
-    k++;
-    let obj = dir.stat(path.resolve(__dirname, "./secret-folder/", String(filenames[k])));
-    obj.then(function b(el) {str[k] += String(el.size); });
-});
-for (let i = 0; i<=k; i++)
-    {
-    
-    console.log(str[i]);
+    for (let i = 0; i <= str.length - 1; i++) {
+        fs.stat(path.resolve(__dirname, "./secret-folder/", String(filenames[i])), function (err, stats) {
+            if (stats.isFile())
+                console.log(str[i] + (stats["size"]/1000).toString()+" kb");
+        });
     }
+});
