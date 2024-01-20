7acbbe30b8a0618705a0923214dcafd2b29d3d92
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-20 14:35:52 +0300

    feat: add solution 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const path = require('node:path');
+const dir = require('node:fs').promises;
+const fs = require('fs');
+
+let pathCss = path.resolve(__dirname, "./project-dist/bundle.css");
+const newfile = new fs.WriteStream(pathCss);
+
+pathCss = path.resolve(__dirname, "./styles");
+const files = dir.readdir(pathCss);
+files.then(function file(data) {
+    let res = "";
+    for (let i = 0; i < data.length; i++) {
+        fs.stat(path.resolve(pathCss, data[i]), function (err, stats) {
+            if (stats.isFile() && path.extname(data[i]) === '.css') {
+                let fileTemp = new fs.ReadStream(path.resolve(pathCss, data[i]));
+                fileTemp.on('readable', function () {
+                    while ((dataTemp = fileTemp.read()) != null) {  
+                        newfile.write(dataTemp);
+                    }
+                });
+            }
+        });
+    };  
+});
+
