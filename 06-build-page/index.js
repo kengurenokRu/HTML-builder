facbe05946d08a71fb7fb83d4201ba11d57218db
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-21 21:43:06 +0300

    feat: add style.css in task 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -43,4 +43,24 @@ files.then(function file(data) {
     });
 });
 
+/* Создание стиля */
+
+const newfile = new fs.WriteStream(pathCss);
+pathCss = path.resolve(__dirname, "./styles");
+files = dir.readdir(pathCss);
+files.then(function file(data) {
+    let res = "";
+    for (let i = 0; i < data.length; i++) {
+        fs.stat(path.resolve(pathCss, data[i]), function (err, stats) {
+            if (stats.isFile() && path.extname(data[i]) === '.css') {
+                let fileTemp = new fs.ReadStream(path.resolve(pathCss, data[i]));
+                fileTemp.on('readable', function () {
+                    while ((dataTemp = fileTemp.read()) != null) {
+                        newfile.write(dataTemp);
+                    }
+                });
+            }
+        });
+    };
+});
 
