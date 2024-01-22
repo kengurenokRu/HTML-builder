5fe0b8414bd309c540b42c5dace38fe1b429d518
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-22 11:04:18 +0300

    feat: fix  parts with create folder assets in solution 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,14 +4,71 @@ const fs = require('fs');
 
 let pathCss = path.resolve(__dirname, "./project-dist/style.css");
 let pathHtml = path.resolve(__dirname, "./project-dist/index.html");
-const oldPathAssets = path.resolve(__dirname, "./assets");
-let newPathAssets = path.resolve(__dirname, "./project-dist");
 
-/*копирование диектории assets*/
+
+/*копирование директории assets*/
+let oldPathAssets = path.resolve(__dirname, "./assets");
+let newPathAssets = path.resolve(__dirname, "./project-dist");
 dir.mkdir(newPathAssets, { recursive: true });
-let directories = dir.readdir(newPathAssets);
 newPathAssets = path.resolve(__dirname, "./project-dist/assets");
 dir.mkdir(newPathAssets, { recursive: true });
+
+let folders = dir.readdir(oldPathAssets, { withFileTypes: true });
+folders.then(function fild(folder) {
+    let folderNameTemp = [];
+    for (let k = 0; k <= folder.length - 1; k++) {
+        if (folder[k].isDirectory()) {
+            folderNameTemp.push(folder[k]);
+            console.log(folder[k]);
+        }
+    }
+    for (let k = 0; k <= folderNameTemp.length - 1; k++) {
+        dir.mkdir(path.resolve(newPathAssets, folderNameTemp[k].name), { recursive: true });
+    }
+   /* for (let k = 0; k <= folderNameTemp.length - 1; k++) {
+       
+       let files = dir.readdir(folderNameTemp[k].path);        
+        files.then(function file(data) {
+            for (let i = 0; i < data.length; i += 1) {
+                fs.copyFile(path.resolve(folderNameTemp[k].path, data[i]), path.resolve(newPathAssets, data[i]), (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+        });
+        let pathes = [];
+        const files2 = dir.readdir(newPathAssets);
+        files.then(function file(data) {
+            files2.then(function file(data2) {
+                for (let i = 0; i < data2.length; i++) {
+                    let k = -1;
+                    for (let j = 0; j < data.length; j++) {
+                        if (data2[i] == data[j]) {
+                            k = j;
+                            break;
+                        }
+                    }
+                    if (k === -1) {
+                        pathes.push(data2[i]);
+                    }
+                }
+                for (let i = 0; i < pathes.length; i++) {
+                    fs.unlink(path.resolve(newPathAssets, pathes[i]), (err) => {
+                        if (err) throw err;
+                    });
+                }
+            });
+        });
+
+    }*/
+});
+
+
+
+
+
+/*
 let files = dir.readdir(oldPathAssets);
 files.then(function file(data) {
     for (let i = 0; i < data.length; i += 1) {
@@ -44,7 +101,7 @@ files.then(function file(data) {
             });
         }
     });
-});
+});*/
 
 /* Создание стиля */
 
@@ -75,10 +132,9 @@ pathHtml1 = path.resolve(__dirname, "template.html");
 let text = ""
 file = new fs.ReadStream(pathHtml1);
 file.on('readable', function () {
-    let data;    
-    while ((data = file.read()) != null) 
-    {
-        text+="\n"+data;
+    let data;
+    while ((data = file.read()) != null) {
+        text += "\n" + data;
     }
 });
 
@@ -92,10 +148,10 @@ files.then(function file(data) {
                 let fileTemp = new fs.ReadStream(path.resolve(pathHtml, data[i]));
                 fileTemp.on('readable', function () {
                     while ((dataTemp = fileTemp.read()) != null) {
-                        tempData+=dataTemp;
+                        tempData += dataTemp;
                     }
                 });
-                text.replace("{{"+path.basename(data[i].name, path.extname(data[i].name))+"}}", tempData);
+                text.replace("{{" + path.basename(data[i].name, path.extname(data[i].name)) + "}}", tempData);
             }
         });
     };
