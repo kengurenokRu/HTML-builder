a31965632bc6b757e20fa0ca6ddbe19d72eef1d5
kengurenokRu <nastassiadunaeva@gmail.com>
2024-01-24 09:38:07 +0300

    feat: fix some bugs and error

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,13 +1,19 @@
 const path = require('node:path');
 const fs = require('node:fs');
 const file = new fs.ReadStream(path.format({ dir: __dirname, base: 'text.txt' }));
+
 let count = 0;
-file.on('readable', function () {
-    let data;    
-    while ((data = file.read()) != null) 
-    {
-        console.log(String(data));
-        count++;        
+file.on('error', function (err) {
+    if (err) console.log("Ошибка чтения или доступа к файлу");
+})
+file.on('readable', function (err) {
+    if (err) console.log("Непредвиденная ошибка.");
+    else {
+        let data;
+        while ((data = file.read()) != null) {
+            console.log(String(data));
+            count++;
+        }
+        if (count == 0) console.log("Файл пуст");
     }
-    if (count == 0) console.log("Файл пуст");
 });
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,17 +5,29 @@ const fs = require('fs');
 const files = dir.readdir(myPath, { withFileTypes: true });
 let str = [];
 let filenames = [];
-files.then(function file(data) {
-    for (let i=0; i<data.length; i++)
-    {
-        str.push(path.basename(data[i].name, path.extname(data[i].name)) + " - " + path.extname(data[i].name).slice(1) + " - ");
-        filenames.push(path.basename(data[i].name));
-    };
-    for (let i = 0; i <= str.length - 1; i++) {
-        fs.stat(path.resolve(__dirname, "./secret-folder/", String(filenames[i])), function (err, stats) {
-            if (stats.isFile())
-                console.log(str[i] + (stats["size"]/1000).toString()+" kb");
-        });
+files.then(function file(data, err) {
+    if (err) console.log("Непредвиденная ошибка.");
+    else {
+        if (data.length === 0) console.log("Директория пуста");
+        else {
+            for (let i = 0; i < data.length; i++) {
+                str.push(path.basename(data[i].name, path.extname(data[i].name)) + " - " + path.extname(data[i].name).slice(1) + " - ");
+                filenames.push(path.basename(data[i].name));
+            };
+            let count = 0;
+            for (let i = 0; i <= str.length - 1; i++) {
+                fs.stat(path.resolve(__dirname, "./secret-folder/", String(filenames[i])), function (err, stats) {
+                    if (err) console.log("Непредвиденная ошибка.");
+                    else {
+                        if (stats.isFile()) {
+                            count++;
+                            console.log(str[i] + (stats["size"]).toString() + " бит");
+                        }
+                    }
+                });
+            }
+            if (count === 0) console.log("Файлов в директории нет");
+        }
     }
 });
 
